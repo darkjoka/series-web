23723f4e17d6d14b794a6f3bd2e9de168163df99
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-11 22:20:12 +0000

    add search to shared app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 
 import { Hero } from "../components/Hero";
 import { TopNav } from "../components/TopNav";
+import { Search } from "../components/Search";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <TopNav />
       <Hero />
+      <Search/>
       <Component {...pageProps} />;
     </>
   );
