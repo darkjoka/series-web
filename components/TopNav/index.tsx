a437f0239fbfe3f93c7235c49acae7844aee7ed1
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-11 21:12:07 +0000

    Initialize top nav

---
+++ components/TopNav/index.tsx
@@ -0,0 +1,5 @@
+import { Wrapper } from "./style";
+
+export const TopNav = () => {
+  return <Wrapper></Wrapper>;
+};
---
+++ components/TopNav/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.nav`
+  height: 64px;
+  border: 2px solid gray;
+`;
