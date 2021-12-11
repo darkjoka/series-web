ec747720d41865ae96351d5c04602bf21fcbbb0b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-11 23:03:48 +0000

    add content to shared app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import "../styles/globals.css";
 import { Hero } from "../components/Hero";
 import { TopNav } from "../components/TopNav";
 import { Search } from "../components/Search";
+import { Content } from "../components/Content";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <TopNav />
       <Hero />
-      <Search/>
-      <Component {...pageProps} />;
+      <Search />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
     </>
   );
 }
