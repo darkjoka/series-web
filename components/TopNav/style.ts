99cc49055c08225e3a318a4c301bc5d439e329df
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-12 13:59:19 +0000

    stick topnav to page

--- components/TopNav/style.ts
+++ components/TopNav/style.ts
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.nav`
   height: 64px;
+  width: 100%;
   border: 2px solid gray;
+  position: fixed;
+  background-color: white;
 `;
