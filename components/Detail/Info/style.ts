b733b6ec64562d2c3f9929932efe3e1a6834c918
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-14 20:56:30 +0000

    update style for genres

--- components/Detail/Info/style.ts
+++ components/Detail/Info/style.ts
@@ -14,22 +14,39 @@ export const Wrapper = styled.div`
 `;
 
 export const Genre = styled.div`
-  background-color: gray;
-  height: 50px;
-  width: 120px;
   margin: 2px;
+  padding: 10px;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 40px;
+    height: 10px;
+    background-color: lightgray;
+    bottom: 8px;
+    left: 6px;
+    z-index: -1;
+    transition: all 0.5s;
+  }
+
+  &:hover {
+    &::before {
+      width: 60px;
+    }
+  }
 `;
 
 export const GenreArea = styled.div`
   border-radius: 2px;
-  border: 2px solid gray;
+  /* border: 2px solid gray; */
   display: flex;
 `;
 
 export const Description = styled.div`
-  height: 180px;
   width: 100%;
   background-color: gray;
   margin-top: 10px;
   border-radius: 2px;
+  padding: 10px 0;
 `;
