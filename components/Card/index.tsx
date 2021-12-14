a8b2e02343736a541b41dc496c1c73f8c4dff97a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-14 19:03:07 +0000

    consume  incoming props

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,10 +1,18 @@
-import { Content, Teaser, Wrapper } from "./style";
+import { CardProps } from "../../shared/constants/types";
+import { Content, Description, Rating, Teaser, Title, Wrapper } from "./style";
 
-export const Card = () => {
+export const Card: React.FC<CardProps> = ({ title, teaser, rating, ...other }) => {
   return (
     <Wrapper>
       <Teaser />
-      <Content></Content>
+      <Content>
+        <Title>{title}</Title>
+        <Description>
+          {teaser.slice(0, 80)}
+          {teaser.length > 80 ? "..." : ""}
+        </Description>
+        <Rating>{(Number(rating) * 2).toFixed(1).toString()}</Rating>
+      </Content>
     </Wrapper>
   );
 };
