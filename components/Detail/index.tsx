f1cbcc2f8ca398e96e12d889e3b3e788e70188d8
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-14 20:50:06 +0000

    use season head as indexer

--- components/Detail/index.tsx
+++ components/Detail/index.tsx
@@ -1,14 +1,15 @@
+import { DetailProps } from "../../shared/constants/types";
 import { Accordion } from "./Accordion";
 import { Hero } from "./Hero";
 import { Info } from "./Info";
 
-export const Detail = ({ description, title, hero, seasonEpisodes, genres }) => {
+export const Detail: React.FC<DetailProps> = ({ description, title, hero, seasonEpisodes, genres }) => {
   return (
     <>
       <Hero hero={hero} />
       <Info description={description} genres={genres} title={title} />
       {seasonEpisodes.map((data, index) => {
-        return <Accordion index={index} key={index} value={null} {...data} />;
+        return <Accordion index={index} key={data.season} value={null} {...data} />;
       })}
     </>
   );
