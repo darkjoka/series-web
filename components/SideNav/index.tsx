9682ae7bcf638e5b6ff3cbc78a7e20cb6b7b9090
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-17 19:41:35 +0000

    add filter to side nav

--- components/SideNav/index.tsx
+++ components/SideNav/index.tsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { Filter } from "../Filter";
+
 import { BackDrop, Nav, Wrapper } from "./style";
 
-export const SideNav = () => {
+export const SideNav = ({ handler }: { handler: () => void }) => {
+  const ref = React.useRef(null);
   return (
     <>
       <Wrapper>
-        <Nav />
-        <BackDrop />
+        <Nav ref={ref}>
+          <Filter parent={ref} handler={handler} />
+        </Nav>
+        <BackDrop onClick={handler} />
       </Wrapper>
     </>
   );
