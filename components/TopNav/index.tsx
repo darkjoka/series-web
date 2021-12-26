fbbfed8b4addf304304c996f4b57f7fbcbb0b575
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-26 15:10:20 +0000

    add icons

--- components/TopNav/index.tsx
+++ components/TopNav/index.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+import { Icon } from "../Icon";
 import { Hamburger, Logo, Main, NavLinks, ThemeIcon, Wrapper } from "./style";
 
 export const TopNav = ({ handler }: { handler: () => void }) => {
   return (
     <Wrapper>
       <Main>
-        <Hamburger onClick={handler} />
-        <Logo />
+        <Hamburger onClick={handler}>
+          <Icon icon="menu-outline" />
+        </Hamburger>
+        <Link href="/" passHref>
+          <Logo>LOGO</Logo>
+        </Link>
         <NavLinks />
-        <ThemeIcon />
+        <ThemeIcon onClick={handler}>
+          <Icon icon="moon-outline" />
+        </ThemeIcon>
       </Main>
     </Wrapper>
   );
