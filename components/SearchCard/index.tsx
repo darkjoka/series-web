84c94f8efebcd5a775f03d000ad1194af319e236
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-27 02:16:30 +0000

    route search results to detail

--- components/SearchCard/index.tsx
+++ components/SearchCard/index.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
 import { SearchCardProps } from "../../shared/constants/types";
 import { Wrapper } from "./style";
 
 export const SearchCard = ({ title, permalink }: SearchCardProps) => {
-  return <Wrapper>{title}</Wrapper>;
+  return (
+    <Link href={`/detail/${permalink}`} passHref>
+      <Wrapper>{title}</Wrapper>
+    </Link>
+  );
 };
