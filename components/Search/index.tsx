eeee9d66f2a6006c0b9a8f7e7cb2e0a2a478b80a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-27 01:13:42 +0000

    add search handlers

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,17 +1,34 @@
+import { useRouter } from "next/router";
+import React from "react";
 import { Icon } from "../Icon";
 import { Button, Clear, Form, Input, Wrapper } from "./style";
 
 export const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const router = useRouter();
+
+  const handleSearch = (event) => setSearchTerm(event.target.value);
+  const handleClear = () => setSearchTerm("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchTerm) {
+      router.push(`/search/${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Wrapper>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input>
-          <input type="text" name="search" id="search" minLength={3} />
-          <Clear>
-            <Icon icon="close-outline" />
-          </Clear>
+          <input type="text" name="search" id="search" minLength={3} value={searchTerm} onChange={handleSearch} />
+          {searchTerm && (
+            <Clear onClick={handleClear}>
+              <Icon icon="close-outline" />
+            </Clear>
+          )}
         </Input>
-        <Button>Search</Button>
+        <Button onClick={handleSubmit}>Search</Button>
       </Form>
     </Wrapper>
   );
