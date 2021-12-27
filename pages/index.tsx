4f9968517da046fd5b7d9eb5630aec6c0bdd2e29
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-27 00:30:54 +0000

    add new load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,65 @@
 import axios from "axios";
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import React from "react";
+import useSWR from "swr";
 import { Card } from "../components/Card";
 import { CardProps } from "../shared/constants/types";
+import { useObserver } from "../shared/hooks/useObserver";
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await axios.get(process.env.BACKEND_HOST);
   return {
     props: {
-      data: response.data.data,
+      info: response.data.data,
     },
   };
 };
 
-export default function Home({ data }: { data: [CardProps] }) {
+const fetcher = (url: string) => url && axios.get(url).then((response) => response.data.data);
+
+export default function Home({ info }: { info: [CardProps] }) {
+  const [ref, onScreen, observer] = useObserver({});
+  const [cursor, setCursor]: any = React.useState(info.length);
+  const [series, setSeries]: any = React.useState(info);
+  const [loadError, setLoadError] = React.useState(false);
+
+  const loadData = onScreen && !loadError;
+
+  const { data, error } = useSWR<[CardProps], any>(
+    loadData ? `${process.env.NEXT_PUBLIC_BACKEND_HOST}/${cursor}` : "",
+    fetcher
+  );
+
+  if (data) {
+    const newSeries = series.concat(data);
+    setCursor(newSeries.length);
+    setSeries(newSeries);
+  }
+
+  if (error) {
+    setLoadError(true);
+  }
+
   return (
     <>
       <Head>
         <title>series App</title>
       </Head>
-      {data.map((info) => (
+      {series.map((info: CardProps) => (
         <Card key={info.title} {...info} />
       ))}
+      {loadError ? (
+        <div
+          onClick={() => {
+            setLoadError(false);
+          }}
+        >
+          An Error occurred. Click to try again
+        </div>
+      ) : (
+        <div ref={ref}>Loading...</div>
+      )}
     </>
   );
 }
