9484398ef8c4564954e45443c015421100a3b419
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-28 00:07:14 +0000

    make theme accessible via provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { AppProps } from "next/app";
 import React, { ReactElement, ReactNode } from "react";
 import NextNProgress from "nextjs-progressbar";
+import { ThemeProvider } from "styled-components";
 
 import "../styles/globals.css";
 
@@ -11,6 +12,8 @@ import { Search } from "../components/Search";
 import { Content } from "../components/Content";
 import { SideNav } from "../components/SideNav";
 import { Portal } from "../components/Portal";
+import { Wrapper } from "../components/Wrapper";
+import useStore from "../store/useStore";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -24,32 +27,45 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   const [isLight, setIsLight] = React.useState<boolean>(true);
 
+  const [toggleLight, toggleDark] = useStore((state) => [state.toggleLight, state.toggleDark]);
+  const theme = useStore((state) => state.theme);
+
   const openSide = () => setIsOpen(true);
   const closeSide = () => setIsOpen(false);
-  const light = () => setIsLight(true);
-  const dark = () => setIsLight(false);
+
+  const light = () => {
+    setIsLight(true);
+    toggleLight();
+  };
+
+  const dark = () => {
+    setIsLight(false);
+    toggleDark();
+  };
 
   const getLayout =
     Component.getLayout ??
     ((page: ReactElement) => (
-      <>
+      <ThemeProvider theme={theme}>
         <Portal selector={"#top"}>
           <TopNav handler={openSide} themeHandlerLight={light} themeHandlerDark={dark} isLight={isLight} />
         </Portal>
         <Portal selector={"#side"}>
           <SideNav handler={closeSide} visible={isOpen} />
         </Portal>
-        <Hero />
-        <Search />
-        <Content>{page}</Content>
-      </>
+        <Wrapper>
+          <Hero />
+          <Search />
+          <Content>{page}</Content>
+        </Wrapper>
+      </ThemeProvider>
     ));
 
   return getLayout(
-    <>
-      <NextNProgress color="#444" options={{ showSpinner: false }} />
+    <ThemeProvider theme={theme}>
+      <NextNProgress color={theme.brand} options={{ showSpinner: false }} />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 };
 
