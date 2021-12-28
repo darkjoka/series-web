69578223ae58a95c67c6e8bc97b1dbced46c1098
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-28 00:25:09 +0000

    apply theme

--- components/SearchCard/style.ts
+++ components/SearchCard/style.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.a`
-  border: 2px solid gray;
   padding: 20px;
   height: 30px;
   border-radius: 2px;
   margin: 8px;
   display: flex;
   align-items: center;
+  box-shadow: ${({ theme }) =>
+    `0px 6px ${16}px ${-3.2 / theme.shadowStrength}px ${theme.shadow},3px -3px 0px 0px ${theme.brand}`};
 `;
