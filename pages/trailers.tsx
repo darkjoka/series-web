a2a25e0bb4e42a964227af2f84e71dd25f3424d6
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-04-17 14:58:01 +0100

    add trailer dialog

--- pages/trailers.tsx
+++ pages/trailers.tsx
@@ -1,12 +1,21 @@
 import axios from "axios";
 import { GetStaticProps } from "next";
+import { useState } from "react";
 
+import { Dialog } from "../components/Dialog";
 import { Heading } from "../components/Heading";
 import { Meta } from "../components/Meta";
 import { WideCard } from "../components/WideCard";
 import { TrailerProps } from "../shared/constants/types";
 
 const dayInSeconds = 60 * 60 * 24;
+
+interface PageProps {
+  data: [TrailerProps];
+  metaImage: string;
+  keywords: string;
+  setDialog: (value: boolean) => void;
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await axios.get(`${process.env.BACKEND_HOST}trailers`);
@@ -29,15 +38,22 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default function App({
-  data,
-  metaImage,
-  keywords,
-}: {
-  data: [TrailerProps];
-  metaImage: string;
-  keywords: string;
-}) {
+export default function App({ data, metaImage, keywords, setDialog }: PageProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [currentTrailer, setCurrentTrailer] = useState("");
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+    setCurrentTrailer("");
+    setDialog(false);
+  };
+
+  const openDialog = (trailer: string) => {
+    setCurrentTrailer(trailer);
+    setIsDialogOpen(true);
+    setDialog(true);
+  };
+
   return (
     <>
       <Meta
@@ -48,8 +64,9 @@ export default function App({
       />
       <Heading>Trailers</Heading>
       {data.map((info) => (
-        <WideCard key={info.permalink} {...info} />
+        <WideCard key={info.permalink} {...info} open={openDialog} />
       ))}
+      <Dialog isOpen={isDialogOpen} trailer={currentTrailer} close={closeDialog} />
     </>
   );
 }
