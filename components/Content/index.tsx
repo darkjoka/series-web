510ebed5e06ec5350ae3e27552edb485670a5881
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-20 11:56:16 +0000

    expose content

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,13 +1,1 @@
-import React from "react";
-
-import { Misc } from "../Misc";
-import { Main, Wrapper } from "./style";
-
-export const Content = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Wrapper>
-      <Misc />
-      <Main>{children}</Main>
-    </Wrapper>
-  );
-};
+export {default} from './Content';
