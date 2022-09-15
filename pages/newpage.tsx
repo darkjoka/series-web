9b47b9a43db87ace2e30642f853396058997c29c
darkjoka <junioropokuansah@gmail.com>
2022-09-15 13:16:46 -0500

    fix return

--- pages/newpage.tsx
+++ pages/newpage.tsx
@@ -2,8 +2,7 @@ import React, { ReactElement } from 'react'
 
 export default function NewPage() {
     //  Top level navigation
-    return 
-    <div className='h-20 bg-white w-full border-b flex items-center justify-between px-6'>
+    return <div className='h-20 bg-white w-full border-b flex items-center justify-between px-6'>
         <div className='w-12 h-12 rounded-lg bg-gray-200'>
 
         </div>
