129b9d6338ea14f068135aae6ae691feda3f2abe
darkjoka <junioropokuansah@gmail.com>
2022-09-16 12:43:27 -0500

    add responsiveness to top nav in new page

--- pages/newpage.tsx
+++ pages/newpage.tsx
@@ -3,18 +3,24 @@ import React, { ReactElement } from 'react'
 export default function NewPage() {
     //  Top level navigation
     return <div className='h-20 bg-white w-full border-b flex items-center justify-between px-6'>
-        <div className='w-12 h-12 rounded-lg bg-gray-200'>
+        <div className='flex items-center space-x-5'>
+            <div className='w-12 h-12 rounded-lg bg-gray-200'>
 
+            </div>
+            <div className='w-[28rem] h-12 rounded-full bg-gray-200 hidden md:block'>
+                
+            </div>
         </div>
+
         <div className='flex items-center justify-between space-x-3'>
-        <div className='w-12 h-12 rounded-full bg-gray-200'>
-
-        </div>        
+            <div className='h-12 w-28 rounded bg-gray-200 hidden lg:block'></div>
         <div className='w-12 h-12 rounded-full bg-gray-200'>
         </div>        
         <div className='w-12 h-12 rounded-full bg-gray-200'>
+        </div>        
+        <div className='w-12 h-12 rounded-full bg-gray-200'>
 
-    </div>        
+        </div>        
 
         </div>
     </div>
