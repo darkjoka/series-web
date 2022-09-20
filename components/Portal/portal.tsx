d8ad9c4d4c1a7c0710575f174ce5fc350b760371
darkjoka <junioropokuansah@gmail.com>
2022-09-20 15:52:09 -0500

    rename portal

--- components/Portal/portal.tsx
+++ components/Portal/portal.tsx
@@ -1,12 +1,10 @@
 // ref: https://spectrum.chat/react-spring/general/react-spring-modal-help~64fcff35-fc8a-4334-b65a-a8d74aa1a552
-import React,{ useEffect, useRef } from "react";
-import { createPortal } from "react-dom";
+import React, {useEffect, useRef} from 'react';
+import {createPortal} from 'react-dom';
 
-
-const portalRoot = document.getElementById("top");
-
-const Portal = ({ children }) => {
-  const mainDivRef = useRef(document.createElement("div"));
+export default function Portal({children}) {
+  const portalRoot = document.getElementById('top');
+  const mainDivRef = useRef(document.createElement('div'));
 
   useEffect(() => {
     const mainDiv = mainDivRef.current;
@@ -21,6 +19,4 @@ const Portal = ({ children }) => {
   }, []);
 
   return mainDivRef.current ? createPortal(children, mainDivRef.current) : null;
-};
-
-export default Portal
+}
