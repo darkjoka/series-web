34cc342c0329c95f35a7cec4e7ab81f7844f25a6
darkjoka <junioropokuansah@gmail.com>
2022-09-22 13:13:45 -0500

    remove unused code

--- components/Heading/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {ReactNode} from 'react';
-import {Wrapper} from './styles';
-
-export const Heading = ({children}: {children: ReactNode}) => (
-  <div className="font-bold text-3xl my-5 w-full text-center ">{children}</div>
-);
