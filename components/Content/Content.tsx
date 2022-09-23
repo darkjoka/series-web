ef9ed84807656141b7d53c530f86b3d2d86e6b83
darkjoka <junioropokuansah@gmail.com>
2022-09-22 23:23:08 -0500

    pad top of conent

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Content = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center pt-24">
       <div className="flex flex-col space-y-4 px-3 sm:flex-row sm:flex-wrap sm:space-x-5 sm:space-y-5 justify-center lg:max-w-[100rem]">
         {children}
       </div>
