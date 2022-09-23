321c6e2a8f1c0c2debfff8d2740d08565bb85f4b
darkjoka <junioropokuansah@gmail.com>
2022-09-22 23:20:46 -0500

    simplify document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,15 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import {Head, Html, Main, NextScript} from 'next/document';
+import React from 'react';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-            <div id="top" />
-            <div id="side" />
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <div id="portal" />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
