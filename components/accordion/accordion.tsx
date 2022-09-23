1dc7ed2843bdd3be30eee2fa2045a1cc1bf1ee5d
darkjoka <junioropokuansah@gmail.com>
2022-09-22 23:16:50 -0500

    add title to accordion

--- components/accordion/accordion.tsx
+++ components/accordion/accordion.tsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
 import {a, useSpring} from '@react-spring/web';
+import React, {ReactNode, useState} from 'react';
+import {ChevronDown} from 'react-feather';
 
-export default function Accordion() {
+export default function Accordion(props: {children: ReactNode; title: string}) {
+  const {children, title} = props;
   const [isOpen, setOpen] = useState(false);
   const {height, opacity} = useSpring({
     from: {height: 0, opacity: 0},
@@ -9,22 +11,31 @@ export default function Accordion() {
   });
   return (
     <div>
-      <a.div
-        className="w-full h-12 rounded-lg rounded-b-none border bg-gray-50 hover:bg-gray-100"
+      <a.button
+        className="w-full h-12 rounded-lg rounded-b-none border bg-gray-50 hover:bg-gray-100 flex items-center px-2 justify-between"
         style={{
           borderBottomRightRadius: height.to({range: [0, 384], output: [8, 0]}),
           borderBottomLeftRadius: height.to({range: [0, 96], output: [8, 0]}),
         }}
         onClick={() => setOpen(!isOpen)}
-      ></a.div>
+      >
+        <p>{title}</p>
+        <ChevronDown
+          className={`${
+            isOpen && 'rotate-180'
+          } transition-all ease-in-out duration-500`}
+        />
+      </a.button>
       {/* Accordion body */}
       <a.div
-        className="w-full border bg-white border-t-0 rounded-b-lg"
+        className="w-full border bg-white border-t-0 rounded-b-lg overflow-auto px-6 divide-y"
         style={{
           height,
           opacity: opacity.to({range: [0, 0.01], output: [0, 1]}),
         }}
-      ></a.div>
+      >
+        {children}
+      </a.div>
     </div>
   );
 }
