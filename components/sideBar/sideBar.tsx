33febdf8b61e9013580832a0c124b1d358cd4465
darkjoka <junioropokuansah@gmail.com>
2022-09-22 23:27:22 -0500

    hook side bar to state

--- components/sideBar/sideBar.tsx
+++ components/sideBar/sideBar.tsx
@@ -2,13 +2,13 @@ import {a, useTransition} from '@react-spring/web';
 import React from 'react';
 
 import dynamic from 'next/dynamic';
+import useSide from '../../store/useSide';
 
 const Portal = dynamic(() => import('../portal'), {ssr: false});
 
-export type SideBarProps = {show: boolean; close: VoidFunction};
-
-export default function SideBar({show, close}: SideBarProps) {
-  const modalTransition = useTransition(show, {
+export default function SideBar() {
+  const {isSideOpen, closeSide} = useSide();
+  const modalTransition = useTransition(isSideOpen, {
     from: {opacity: 0, x: 288},
     enter: {opacity: 1, x: 0},
     leave: {opacity: 0, x: 288},
@@ -24,7 +24,7 @@ export default function SideBar({show, close}: SideBarProps) {
                 <a.div
                   className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30 z-10"
                   style={{opacity}}
-                  onClick={close}
+                  onClick={closeSide}
                 ></a.div>
                 <a.div
                   className="fixed w-72 h-full bg-white z-10 right-0"
