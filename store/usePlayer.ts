55f84f9220ad432ddb263d5047e6d095165436ea
darkjoka <junioropokuansah@gmail.com>
2022-09-22 22:21:05 -0500

    update store

--- store/usePlayer.ts
+++ store/usePlayer.ts
@@ -2,9 +2,25 @@ import useStore, { Store } from './useStore'
 import { stop, resume } from '../utils/bodyscroll'
 
 
-const selector = ({ isPlayerOpen, closePlayer, openPlayer }: Store) => { return { isPlayerOpen, closePlayer, openPlayer } }
+const selector = ({ isPlayerOpen, closePlayer, openPlayer, playerSrc, setPlayer }: Store) => {
+    return {
+        isPlayerOpen,
+        closePlayer,
+        openPlayer,
+        playerSrc,
+        setPlayer
+    }
+}
 
 export default function usePlayer() {
-    const { isPlayerOpen, closePlayer, openPlayer } = useStore(selector)
-    return { isPlayerOpen, closePlayer: () => stop(closePlayer), openPlayer: () => resume(openPlayer) }
+    const { isPlayerOpen, closePlayer,
+        openPlayer, playerSrc, setPlayer } = useStore(selector)
+
+    return {
+        isPlayerOpen,
+        closePlayer: () => stop(closePlayer),
+        openPlayer: () => resume(openPlayer),
+        playerSrc,
+        setPlayer
+    }
 }
--- store/useStore.ts
+++ store/useStore.ts
@@ -5,12 +5,14 @@ const useStore = create<Store>((set) => ({
   isPlayerOpen: false,
   isSearchOpen: false,
   isAnyOpen: false,
-  openSide: ()=> set((state)=> ({isSideOpen: true, isAnyOpen: true})),
-  closeSide: ()=> set((state)=> ({isSideOpen: false, isAnyOpen: false})),
-  openPlayer: ()=> set((state)=> ({isPlayerOpen: true, isAnyOpen: true})),
-  closePlayer: ()=> set((state)=> ({isPlayerOpen: false, isAnyOpen: false})),
-  openSearch: ()=> set((state)=> ({isSearchOpen: true, isAnyOpen: true})),
-  closeSearch: ()=> set((state)=> ({isSearchOpen: false, isAnyOpen: false})),
+  playerSrc: "",
+  openSide: () => set((state) => ({ isSideOpen: true, isAnyOpen: true })),
+  closeSide: () => set((state) => ({ isSideOpen: false, isAnyOpen: false })),
+  openPlayer: () => set((state) => ({ isPlayerOpen: true, isAnyOpen: true })),
+  closePlayer: () => set((state) => ({ isPlayerOpen: false, isAnyOpen: false, playerSrc: "" })),
+  openSearch: () => set((state) => ({ isSearchOpen: true, isAnyOpen: true })),
+  closeSearch: () => set((state) => ({ isSearchOpen: false, isAnyOpen: false })),
+  setPlayer: (source: string) => set((state) => ({ playerSrc: source }))
 }));
 
 
@@ -19,12 +21,14 @@ export type Store = {
   isPlayerOpen: boolean
   isSearchOpen: boolean
   isAnyOpen: boolean
+  playerSrc: string
   openSide: VoidFunction
   closeSide: VoidFunction
   openPlayer: VoidFunction
   closePlayer: VoidFunction
   openSearch: VoidFunction
   closeSearch: VoidFunction
+  setPlayer: (source: string) => void
 
 }
 
