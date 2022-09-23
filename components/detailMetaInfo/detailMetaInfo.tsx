c6a98e51f80a162e18a211fa1d23699c3b26b0e9
darkjoka <junioropokuansah@gmail.com>
2022-09-22 23:24:41 -0500

    use actual values except for last aired

--- components/detailMetaInfo/detailMetaInfo.tsx
+++ components/detailMetaInfo/detailMetaInfo.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import {Share2} from 'react-feather';
 
-export default function DetailMetaInfo() {
+export default function DetailMetaInfo({title, handleShare}) {
   return (
     <div className="space-y-4">
       <div className="flex justify-between w-full items-center">
         <div>
           <p>19 September 2021</p>
-          <p className="text-sm text-gray-600">Last aired</p>
+          <p className="text-sm text-gray-400">Last aired</p>
         </div>
-        <button className="p-2 border rounded space-x-2 flex items-center justify-around hover:bg-gray-100">
+        <button
+          onClick={handleShare}
+          className="p-2 border rounded space-x-2 flex items-center justify-around hover:bg-gray-100"
+        >
           <Share2 />
           <p className="hidden sm:block">Share</p>
         </button>
       </div>
       <div>
-        <p className="font-bold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-          molestias.
-        </p>
+        <p className="font-bold">{title}</p>
       </div>
     </div>
   );
