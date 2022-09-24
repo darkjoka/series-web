a9d52c16b00e782260fea3a4830463a636fe8260
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-09-23 23:46:24 -0500

    overflow genre

--- components/detailDescription/detailDescription.tsx
+++ components/detailDescription/detailDescription.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export default function DetailDescription({description, genres}) {
+export default function DetailDescription({ description, genres }) {
   return (
     <>
       {/* tags */}
-      <div className="flex items-center space-x-2 my-2">
-        {genres.map(genre => (
-          <GenreTag {...{genre}} key={genre} />
+      <div className="flex items-center space-x-2 my-2 overflow-x-scroll">
+        {genres.map((genre) => (
+          <GenreTag {...{ genre }} key={genre} />
         ))}
       </div>
 
@@ -18,6 +18,6 @@ export default function DetailDescription({description, genres}) {
   );
 }
 
-const GenreTag = ({genre}) => {
+const GenreTag = ({ genre }) => {
   return <div className="px-2 border rounded hover:bg-gray-100">{genre}</div>;
 };
