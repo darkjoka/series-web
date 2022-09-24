119e06d00c68a054e89a9897e641732025d314b9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-09-23 20:48:09 -0500

    return div only

--- pages/trailers.tsx
+++ pages/trailers.tsx
@@ -1,54 +1,58 @@
-import axios from 'axios';
-import {GetStaticProps} from 'next';
-import React, {useState} from 'react';
+// import axios from 'axios';
+// import {GetStaticProps} from 'next';
+// import React, {useState} from 'react';
 
-import {Meta} from '../components/Meta';
-import WideCard from '../components/wideCard';
-import {TrailerProps} from '../shared/constants/types';
+// import {Meta} from '../components/Meta';
+// import WideCard from '../components/wideCard';
+// import {TrailerProps} from '../shared/constants/types';
 
-const dayInSeconds = 60 * 60 * 24;
+// const dayInSeconds = 60 * 60 * 24;
 
-interface PageProps {
-  data: [TrailerProps];
-  metaImage: string;
-  keywords: string;
-  setDialog: (value: boolean) => void;
-}
+// interface PageProps {
+//   data: [TrailerProps];
+//   metaImage: string;
+//   keywords: string;
+//   setDialog: (value: boolean) => void;
+// }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios.get(`${process.env.BACKEND_HOST}trailers`);
-  const data: [TrailerProps] = response.data.data;
+// export const getStaticProps: GetStaticProps = async () => {
+//   const response = await axios.get(`${process.env.BACKEND_HOST}trailers`);
+//   const data: [TrailerProps] = response.data.data;
 
-  const metaImage = data[Math.floor(Math.random() * data.length)].thumbnailSrc;
-  const titles = data.map(({title}) => title);
-  const productions = data.map(({production}) => production);
-  const productionSet = new Array(new Set(productions));
-  const keywordArr = [...titles, ...productionSet];
-  const keywords = keywordArr.join(', ');
+//   const metaImage = data[Math.floor(Math.random() * data.length)].thumbnailSrc;
+//   const titles = data.map(({title}) => title);
+//   const productions = data.map(({production}) => production);
+//   const productionSet = new Array(new Set(productions));
+//   const keywordArr = [...titles, ...productionSet];
+//   const keywords = keywordArr.join(', ');
 
-  return {
-    props: {
-      data,
-      metaImage,
-      keywords,
-    },
-    revalidate: dayInSeconds,
-  };
-};
+//   return {
+//     props: {
+//       data,
+//       metaImage,
+//       keywords,
+//     },
+//     revalidate: dayInSeconds,
+//   };
+// };
 
-export default function App({data, metaImage, keywords, setDialog}: PageProps) {
-  return (
-    <>
-      <Meta
-        title="Series App - Trailers"
-        image={metaImage}
-        description="Watch trailers of the latest tv series"
-        keywords={keywords}
-      />
-      <div />
-      {data.map(info => (
-        <WideCard key={info.permalink} {...info} />
-      ))}
-    </>
-  );
+// export default function App({data, metaImage, keywords, setDialog}: PageProps) {
+//   return (
+//     <>
+//       <Meta
+//         title="Series App - Trailers"
+//         image={metaImage}
+//         description="Watch trailers of the latest tv series"
+//         keywords={keywords}
+//       />
+//       <div />
+//       {data.map(info => (
+//         <WideCard key={info.permalink} {...info} />
+//       ))}
+//     </>
+//   );
+// }
+
+export default function Home() {
+  return <div>Welcome</div>;
 }
